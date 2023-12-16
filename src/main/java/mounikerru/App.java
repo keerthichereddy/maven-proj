1e61d48f40159e0ba0712cec02e9ac8270aabd05
pullamma <keerthireddychereddy@gmail.com>
2023-12-16 14:46:07 +0530

    mounishakeerthi

---
+++ src/main/java/mounikerru/App.java
@@ -0,0 +1,13 @@
+package mounikerru;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
